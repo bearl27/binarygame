60eea4cca3957bed3902f365537b5967ca86fcc4
bearl <125728897+bearl27@users.noreply.github.com>
2023-11-20 13:22:30 +0900

    Android

---
+++ banakabe_apple/banakabe_apple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  banakabe_apple
+//
+//  Created by kai on 2023/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
